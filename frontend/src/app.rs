0b336211e4e7c753c6db5e1c5f47753816a31966
Ghislain PUTOIS <ghislain.putois@gmail.com>
2020-05-21 09:22:42 +0200

    A working fetch call example.

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -1,34 +1,91 @@
+use anyhow::Error;
+use serde_derive::{Deserialize, Serialize};
+use yew::format::{Json, Nothing};
 use yew::prelude::*;
+use yew::services::console::ConsoleService;
+use yew::services::fetch::{FetchService, FetchTask, Request, Response};
 
 pub struct App {
     name: String,
-    link:ComponentLink<Self>,
+    link: ComponentLink<Self>,
+    fetch_service: FetchService,
+    console_service: ConsoleService,
+    fetching: bool,
+    data: Option<u32>,
+    ft: Option<FetchTask>,
 }
 
 pub enum Msg {
     NoOp,
+    FetchData,
+    FetchReady(Result<DataFromFile, Error>),
+    Ignore,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct DataFromFile {
+    value: u32,
 }
 
 impl Component for App {
     type Message = Msg;
     type Properties = ();
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        App { link, name: "toto".to_string()}
+        App {
+            console_service: ConsoleService::new(),
+            fetch_service: FetchService::new(),
+            link,
+            fetching: false,
+            data: None,
+            ft: None,
+            name: "toto".to_string(),
+        }
+    }
+
+    fn rendered(&mut self, first_render: bool) {
+        if first_render {
+            self.link.callback(|_| Msg::FetchData).emit("");
+        }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::NoOp => {},
+            Msg::NoOp => {}
+            Msg::FetchData => {
+                self.fetching = true;
+                let callback = self.link.callback(
+                    move |response: Response<Json<Result<DataFromFile, Error>>>| {
+                        let (meta, Json(data)) = response.into_parts();
+                        if meta.status.is_success() {
+                            Msg::FetchReady(data)
+                        } else {
+                            Msg::Ignore
+                        }
+                    },
+                );
+                let request = Request::get("/data.json").body(Nothing).unwrap();
+                let task = self.fetch_service.fetch(request, callback).unwrap();
+                self.ft = Some(task);
+            }
+            Msg::FetchReady(response) => {
+                self.fetching = false;
+                self.data = response.map(|data| data.value).ok();
+            }
+            Msg::Ignore => {
+                self.fetching = false;
+                self.data = Some(13);
+            }
         }
         true
     }
 
     fn view(&self) -> Html {
         html! {
-            <div>{"Hello world!"}</div>
+            <div>{ "Hello world!, " }{self.fetching}{ format!("{:?}", self.data) }</div>
         }
     }
 
-    fn change(&mut self, _: <Self as yew::html::Component>::Properties) -> bool { false }
-
+    fn change(&mut self, _: <Self as yew::html::Component>::Properties) -> bool {
+        false
+    }
 }
