404c21ab4ca98b09b59728f07bdaa2d6109340c4
Ghislain PUTOIS <ghislain.putois@gmail.com>
2020-07-15 13:20:34 +0200

    rust fmt.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,7 @@
 use actix_files::NamedFile;
-use actix_web::{web, App, HttpRequest, HttpServer, Result, Responder};
-use std::path::PathBuf;
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, Result};
 use serde_json;
+use std::path::PathBuf;
 
 async fn index(req: HttpRequest) -> Result<NamedFile> {
     let mut path: PathBuf = req.match_info().query("filename").parse().unwrap();
@@ -13,22 +13,23 @@ async fn index(req: HttpRequest) -> Result<NamedFile> {
 }
 
 /*
-   async fn get_data(_req: HttpRequest) -> impl Responder {
-   let corpus = std::fs::File::open("/home/ghpu/projets/rust/fullstack/result.json").expect("cannot open result.json");
-   let corpus : common::Corpus = serde_json::from_reader(corpus).unwrap();
+async fn get_data(_req: HttpRequest) -> impl Responder {
+let corpus = std::fs::File::open("/home/ghpu/projets/rust/fullstack/result.json").expect("cannot open result.json");
+let corpus : common::Corpus = serde_json::from_reader(corpus).unwrap();
 
-   web::Json(corpus)
-   }
-   */
-
+web::Json(corpus)
+}
+*/
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new()
-                    //.route("/data", web::get().to(get_data))
-                    .route("/{filename:.*}", web::get().to(index))
-                    .route("", web::get().to(index)))
-        .bind("0.0.0.0:8080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            //.route("/data", web::get().to(get_data))
+            .route("/{filename:.*}", web::get().to(index))
+            .route("", web::get().to(index))
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
