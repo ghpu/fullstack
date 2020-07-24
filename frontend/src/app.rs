955fdf346e84abacad48b2a658f9beb518971876
Ghislain PUTOIS <ghislain.putois@gmail.com>
2020-07-24 11:20:50 +0200

    Display pie has one weighted entry per annotation.

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -531,7 +531,8 @@ impl GraphDisplay {
                     CompareList::LeftVSRight => &current_cases[i].left_vs_right,
                     _ => panic!("not possible"),
                 };
-            for e in what[0..1].iter() {
+
+            for e in what.iter() {
                 let count = hm.entry(*e).or_insert(0);
                 *count +=current_cases[i].count;
             }
